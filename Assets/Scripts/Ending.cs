abe1367c7361117a94d878bfb2644afe01c3cd75
RafaMacias10 <r.m.macias100@gmail.com>
2026-01-28 10:47:50 +0100

    Update Ending

--- Assets/Scripts/Ending.cs
+++ Assets/Scripts/Ending.cs
@@ -1,42 +1,37 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 #if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
+
 public class Ending : MonoBehaviour
 {
     public enum EndingType { Death = 0, Win = 1 }
-
     private static EndingType s_type = EndingType.Death;
 
-    [Header("UI")]
     [SerializeField] private TMP_Text textEnding;
-
-    [Header("Scene Names")]
     [SerializeField] private string restartSceneName = "Gameplay";
 
-    [Header("Messages")]
     [SerializeField] private string deathTitle = "Game Over";
     [SerializeField] private string winTitle = "You Win!";
 
     private void Awake()
     {
-        if (textEnding == null)
-            textEnding = GetComponent<TMP_Text>();
+        if (textEnding == null) textEnding = GetComponent<TMP_Text>();
     }
 
     private void Start()
     {
         Time.timeScale = 1;
 
-        int score = (GameManager.instance != null) ? GameManager.instance.GetTotalCoins() : 0;
+        int score = PlayerPrefs.GetInt("FINAL_SCORE", 0);
         string title = (s_type == EndingType.Win) ? winTitle : deathTitle;
 
         textEnding.text =
             $"{title}\n\n" +
-            $"Monedas: {score}\n\n" +
+            $"Score: {score}\n\n" +
             "Press ENTER to restart\n" +
             "Press ESC to exit";
     }
@@ -45,8 +40,8 @@ public class Ending : MonoBehaviour
     {
         if (IsEnterPressed())
         {
-            if (GameManager.instance != null)
-                GameManager.instance.totalCoins = 0;
+            PlayerPrefs.SetInt("FINAL_SCORE", 0);
+            PlayerPrefs.Save();
 
             Time.timeScale = 1;
             SceneManager.LoadSceneAsync(restartSceneName);
@@ -87,6 +82,3 @@ public class Ending : MonoBehaviour
         return Input.GetKeyDown(KeyCode.Escape);
     }
 }
-
-
-
